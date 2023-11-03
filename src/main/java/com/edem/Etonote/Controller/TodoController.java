9b4a4e7af97afb60bc3bef3f5223af63b9cb0513
heyEdem <akatoaugustine@gmail.com>
2023-11-03 23:56:40 +0000

    Controller

--- src/main/java/com/edem/Etonote/Controller/TodoController.java
+++ src/main/java/com/edem/Etonote/Controller/TodoController.java
@@ -4,25 +4,24 @@ import com.edem.Etonote.Entities.Todo;
 import com.edem.Etonote.Service.TodoServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+
 import java.util.List;
 
-@RestController
-@RequestMapping("/home")
+@Controller
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoServiceImpl service;
 
-    @GetMapping
+    @GetMapping("/")
     public String getAllTodos(Model model){
-        List<Todo> todos  = service.findAllTodos();
-        model.addAttribute("todos",todos);
-        return "index.";
+        service.findAllTodos();
+        return "index";
     }
 
 
@@ -32,22 +31,22 @@ public class TodoController {
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Todo not found"));
         return "single-todo";
     }
-
-
-    @PostMapping
-    public Todo create(@RequestBody Todo todo ){
-        return service.createTodo(todo);
-    }
-
-
-    @PutMapping("/{id}")
-    public void update (@RequestBody Todo todo, @PathVariable Long id ){
-        service.createTodo(todo);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete (@PathVariable Long id, @RequestBody Todo todo){
-         service.deleteTodoById(id);
-    }
+//
+//
+//    @PostMapping
+//    public Todo create(@RequestBody Todo todo ){
+//        return service.createTodo(todo);
+//    }
+//
+//
+//    @PutMapping("/{id}")
+//    public void update (@RequestBody Todo todo, @PathVariable Long id ){
+//        service.createTodo(todo);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete (@PathVariable Long id, @RequestBody Todo todo){
+//         service.deleteTodoById(id);
+//    }
 
 }
