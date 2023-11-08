6fcbd8d88e5b20faa35e41c95d7414417baa8154
heyEdem <akatoaugustine@gmail.com>
2023-11-08 14:15:24 +0000

    Method to create Todo and set the status to uncompleted

--- src/main/java/com/edem/Etonote/MyRunner/MyRunner.java
+++ src/main/java/com/edem/Etonote/MyRunner/MyRunner.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import static com.edem.Etonote.Entities.Status.COMPLETE;
+import static com.edem.Etonote.Entities.Status.UNCOMPLETED;
 
 @Component
 public class MyRunner implements CommandLineRunner {
@@ -23,12 +24,14 @@ public class MyRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        TodoList list1 = todoListService.createList("Urgent");
-        TodoList list2 = todoListService.createList("Urgent");
-        TodoList list3= todoListService.createList("Urgent");
+        TodoList list1 = todoListService.createList("KOD");
+        TodoList list2 = todoListService.createList("4YEO");
+        TodoList list3= todoListService.createList("TOS");
 
-        Todo todo1 = todoService.createTodo("DIY","Do it yourself",COMPLETE,list1.getListId());
-        Todo todo2 = todoService.createTodo("DIY","Do it yourself",COMPLETE,list2.getListId());
-        Todo todo3 = todoService.createTodo("DIY","Do it yourself",COMPLETE,list3.getListId());
+        Todo todo1 = todoService.createTodo("Photograph","Fell in love through photograph",list1.getListId());
+        Todo todo2 = todoService.createTodo("FIENDS","Gimme drinks, Gimme smoke",list1.getListId());
+        Todo todo3 = todoService.createTodo("Vile mentality","Nigga play me, never",list2.getListId());
+        Todo todo4 = todoService.createTodo("Amari","I never fall out with the bros",list3.getListId());
+        Todo todo5 = todoService.createTodo("My Life","It's my life now, my life",list3.getListId());
     }
 }
