487da26b6a710c7960eb449929ee0d974c486676
heyEdem <akatoaugustine@gmail.com>
2023-11-08 20:35:41 +0000

    Controller class

--- src/main/java/com/edem/Etonote/Controller/TodoController.java
+++ src/main/java/com/edem/Etonote/Controller/TodoController.java
@@ -1,52 +1,41 @@
 package com.edem.Etonote.Controller;
 
 import com.edem.Etonote.Entities.Todo;
+import com.edem.Etonote.Entities.TodoList;
+import com.edem.Etonote.Service.TodoListServiceImpl;
 import com.edem.Etonote.Service.TodoServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-
 import java.util.List;
+import java.util.Optional;
 
-@Controller
-@RequiredArgsConstructor
+@RestController
 public class TodoController {
 
-    private final TodoServiceImpl service;
+    private final TodoServiceImpl todoService;
+    private final TodoListServiceImpl listService;
+
+    public TodoController(TodoServiceImpl todoService, TodoListServiceImpl listService) {
+        this.todoService = todoService;
+        this.listService = listService;
+    }
 
     @GetMapping("/")
     public String getAllTodos(Model model){
-        service.findAllTodos();
+        listService.findAllLists();
         return "index";
     }
 
 
-    @GetMapping("/{id}")
-    public String getById(@PathVariable Long id){
-        Todo todo = service.findTodoById(id)
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Todo not found"));
-        return "single-todo";
+    @GetMapping("/{listId}")
+    public String getById(@PathVariable("listId") Long listId, Model model){
+        Optional<TodoList> list = listService.findListById(listId);
+        List<Todo> todos = todoService.findTodoByTodoListId(list.get().getListId());
+        model.addAttribute("todos",todos);
+        return "single-list";
     }
-//
-//
-//    @PostMapping
-//    public Todo create(@RequestBody Todo todo ){
-//        return service.createTodo(todo);
-//    }
-//
-//
-//    @PutMapping("/{id}")
-//    public void update (@RequestBody Todo todo, @PathVariable Long id ){
-//        service.createTodo(todo);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void delete (@PathVariable Long id, @RequestBody Todo todo){
-//         service.deleteTodoById(id);
-//    }
-
+    //TODO: Controller methods
 }
