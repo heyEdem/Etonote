edce74d100bef336f1fcfe4f183fdd0c9a014070
heyEdem <akatoaugustine@gmail.com>
2023-11-09 16:58:43 -0600

    Controller

--- src/main/java/com/edem/Etonote/Controller/TodoController.java
+++ src/main/java/com/edem/Etonote/Controller/TodoController.java
@@ -4,7 +4,7 @@ import com.edem.Etonote.Entities.Todo;
 import com.edem.Etonote.Entities.TodoList;
 import com.edem.Etonote.Service.TodoListServiceImpl;
 import com.edem.Etonote.Service.TodoServiceImpl;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,7 +12,8 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+
+@Controller
 public class TodoController {
 
     private final TodoServiceImpl todoService;
@@ -23,14 +24,15 @@ public class TodoController {
         this.listService = listService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getAllTodos(Model model){
-        listService.findAllLists();
+        List<TodoList> lists = listService.findAllLists();
+        model.addAttribute("allLists",lists);
         return "index";
     }
 
 
-    @GetMapping("/{listId}")
+    @GetMapping("/view/{listId}")
     public String getById(@PathVariable("listId") Long listId, Model model){
         Optional<TodoList> list = listService.findListById(listId);
         List<Todo> todos = todoService.findTodoByTodoListId(list.get().getListId());
