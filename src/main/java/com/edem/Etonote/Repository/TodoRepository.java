4c3756cda511a357a470e33d3cde433f20508ff2
heyEdem <akatoaugustine@gmail.com>
2024-02-13 23:45:27 +0000

    Service- findByStatus

--- src/main/java/com/edem/Etonote/Repository/TodoRepository.java
+++ src/main/java/com/edem/Etonote/Repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package com.edem.Etonote.Repository;
 
+import com.edem.Etonote.Entities.Status;
 import com.edem.Etonote.Entities.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findTodoByTitleContains(String keyword);
 
+    List <Todo> findByStatusOrderByCreatedAt(Status status);
 
     @Query(value ="select * from todos as t where t.todo_id in (select e.todo_id from todo_lists as e where e.todo_list_id=:todoListId)", nativeQuery = true)
     List<Todo> findTodoByTodoListId(@Param("todoListId") Long todoListId);
